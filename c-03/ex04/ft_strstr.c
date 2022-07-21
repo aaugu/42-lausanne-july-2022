0ef34f3c4f837912d1f123f29fbd79bfc59a5fb6
Anais Augu <augu.anais@gmail.com>
2022-07-21 21:56:41 +0200

    ex04 ok

--- c-03/ex04/ft_strstr.c
+++ c-03/ex04/ft_strstr.c
@@ -1,39 +1,66 @@
 #include <stdio.h>
 #include <string.h>
-/*
-char	is_matching()
+
+char	is_matching(char *str, char *to_find, int i)
+{
+  int j;
+
+  j = 0;
+  while (to_find[j] != '\0')
+  {
+    if (to_find[j] == str[i + j])
+      j++;
+    else
+      return (0);
+  }
+  return (1);
+}
 
 char	*ft_strstr(char *str, char *to_find)
 {
-	int	i;
-	int	j;
+	int i;
 
 	i = 0;
-	j = 0;
+  if (to_find[0] == '\0')
+  {
+    return (str);
+  }
+  while (str[i] != '\0')
+  {
+    if (str[i] == to_find[0])
+    {
+      if (is_matching(str, to_find, i))
+      {
+        return &str[i];
+      }
+    }
+    i++;
+  }
+  return (0);
 }
-*/
+
 int	main(void)
 {
 	char	str[] = "Hello World Woohoo!";
 	char	to_find[] = "World";
-	char	to_find1[] = "42";
+//	char	to_find1[] = "42";
 	char	to_find2[] = "";
 	char	to_find3[] = "Woo";
 	char	str1[] = "";
 	char	to_find4[] = "";
 
-//	printf("%s\n", ft_strstr(str, to_find));
-	printf("%s\n", strstr(str, to_find));
+  printf("%s\n", strstr(str, to_find));
+	printf("%s\n", ft_strstr(str, to_find));
+/*	printf("%s\n", "-----");
+	printf("%s\n", ft_strstr(str, to_find1));
+	printf("%s\n", strstr(str, to_find1));*/
 	printf("%s\n", "-----");
-//	printf("%s\n", ft_strstr(str, to_find1));
-	printf("%s\n", strstr(str, to_find1));
-	printf("%s\n", "-----");
-//	printf("%s\n", ft_strstr(str, to_find2));
+  printf("%s\n", ft_strstr(str, to_find2));
 	printf("%s\n", strstr(str, to_find2));
 	printf("%s\n", "-----");
-//	printf("%s\n", ft_strstr(str, to_find3));
+	printf("%s\n", ft_strstr(str, to_find3));
 	printf("%s\n", strstr(str, to_find3));
 	printf("%s\n", "-----");
-//	printf("%s\n", ft_strstr(str1, to_find4));
+	printf("%s\n", ft_strstr(str1, to_find4));
 	printf("%s\n", strstr(str1, to_find4));
 }
