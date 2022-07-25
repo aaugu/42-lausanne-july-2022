d959e79c2c85bccb2f4275c1f8ec5bf31e86d804
Anais Augu <augu.anais@gmail.com>
2022-07-25 21:20:09 +0200

    ex01 c03 corrected

---
+++ c-03/ex01/ft_strncmp.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaugu <marvin@42lausanne.ch>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/25 21:08:43 by aaugu             #+#    #+#             */
+/*   Updated: 2022/07/25 21:08:43 by aaugu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+int ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0' && i < n - 1)
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+int	main(void)
+{
+	char	s1[] = "Hello !";
+	char	s2[] = "Hella World";
+
+	printf("%d\n", strncmp(s1, s2, 4));
+	printf("%d\n", ft_strncmp(s1, s2, 4));
+	printf("%s", "-----\n");
+	printf("%d\n", strncmp(s2, s1, 7));
+	printf("%d\n", ft_strncmp(s2, s1, 7));
+    printf("%s", "-----\n");
+	printf("%d\n", strncmp(s2, s1, 0));
+	printf("%d\n", ft_strncmp(s2, s1, 0));
+}
